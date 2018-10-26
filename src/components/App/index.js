b20e1841544bb218a4df1682ba12415dfc0b6623
Emily Morgado <emilyannemorgado@gmail.com>
2018-10-25 21:06:34 -0700

    Add venue route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'components/Home';
 import Suggestions from 'components/Suggestions';
 import Help from 'components/Help';
+import Venue from 'components/Venue';
 
 
 export default () => {
@@ -13,6 +14,7 @@ export default () => {
           <Route exact path='/' component={Home} />
           <Route path='/suggestions' component={Suggestions} />
           <Route path='/offerhelp' component={Help} />
+          <Route path='/venue' component={Venue} />
         </Switch>
     </BrowserRouter>
   );

883297b825738f85a31d8bd6cc59af3e2b90635f
Emily Morgado <emilyannemorgado@gmail.com>
2018-11-02 07:48:35 -0700

    Update Accommodation file with real information

--- src/components/Accommodation/index.js
+++ src/components/Accommodation/index.js
@@ -6,15 +6,15 @@ const Accommodation = () => {
     <div>
       <h1>Accommodation</h1>
       <p>Looking to stay the night? Here are some suggestions:</p>
-        <ul>
-          <h2>For big groups (14 people):</h2>
-          <li>https://www.airbnb.com/rooms/16351824</li>
-          <li>https://www.airbnb.com/rooms/24992279</li>
-          <li>https://www.airbnb.com/</li>
-          <h3>Nearby Hotels</h3>
-          <li>http://marinainnberkeley.us/</li>
-          <li>http://www.laquintaberkeley.com/?cid=local_6238</li>
-        </ul>
+      <ul>
+        <h2>For big groups (14 people):</h2>
+        <li>https://www.airbnb.com/rooms/16351824</li>
+        <li>https://www.airbnb.com/rooms/24992279</li>
+        <li>https://www.airbnb.com/</li>
+        <h3>Nearby Hotels</h3>
+        <li>http://marinainnberkeley.us/</li>
+        <li>http://www.laquintaberkeley.com/?cid=local_6238</li>
+      </ul>
     </div>
   )
 }

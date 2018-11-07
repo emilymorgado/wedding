7fc6d2b5e810f2f103392cd5617570793798983f
Emily Morgado <emilyannemorgado@gmail.com>
2018-11-07 07:53:44 -0800

    Refactor text area to use react hooks

--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -1,82 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-class Help extends Component {
-  render() {
-    return (
-      <div className='main-container'>
-        <h1>Help/Suggestions/Special Requests</h1>
-        <p>Have ideas for making this a great event? Have special needs? Skills or toys to offer/contribute/lend? Want to help us set up or tear down? We greatly appreciate all that you have to offer.</p>
-        <div>
-          <Form
-            btnName='Let us know!'/>
-        </div>
+const Help = () => {
+  return (
+    <div className='main-container'>
+      <h1>Help/Suggestions/Special Requests</h1>
+      <p>Have ideas for making this a great event? Have special needs? Skills or toys to offer/contribute/lend? Want to help us set up or tear down? We greatly appreciate all that you have to offer.</p>
+      <div>
+        <Form
+          btnName='Let us know!'/>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
 export default Help;
 
 
-class Form extends Component {
-  state = {
-    name: '',
-    email: '',
-    text: '',
+const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [text, setText] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value)
   };
 
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
+  const handleEmailChange = event => {
+    setEmail(event.target.value)
   };
 
-  handleEmailChange = event => {
-    this.setState({ email: event.target.value });
+  const handleTextChange = event => {
+    setText(event.target.value)
   };
 
-  handleTextChange = event => {
-    this.setState({ text: event.target.value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    // if (this.props.reducer === 'suggestion') {
-    //   this.props.saveSuggestion(this.state.text)
-    // } else if (this.props.reducer === 'offer') {
-    //   this.props.saveOffer(this.state.text)
-    // }
-    this.setState({
-      name: '',
-      email: '',
-      text: '',
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} >
-          <textarea
-            className='small-input'
-            placeholder='Name'
-            onChange={this.handleNameChange}
-            value={this.state.name}
-          />
-          <textarea
-            className='small-input'
-            placeholder='Email'
-            onChange={this.handleEmailChange}
-            value={this.state.email}
-          />
-          <textarea
-            placeholder='Your message here...'
-            onChange={this.handleTextChange}
-            value={this.state.text}
-          />
-          <button className={'form-btn'} >{this.props.btnName}</button>
-        </form>
-      </div>
-    )
+    setName('')
+    setEmail('')
+    setText('')
   }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} >
+        <textarea
+          className='small-input'
+          placeholder='Name'
+          onChange={handleNameChange}
+          value={name}
+        />
+        <textarea
+          className='small-input'
+          placeholder='Email'
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <textarea
+          placeholder='Your message here...'
+          onChange={handleTextChange}
+          value={text}
+        />
+        <button className={'form-btn'}>Let Us Know!</button>
+      </form>
+    </div>
+  )
 }
-//If possible, fetch directly in the render()

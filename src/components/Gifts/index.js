a44eabbaf848ae497d2dcb5e0ad543a6f562c7ea
Emily Morgado <emilyannemorgado@gmail.comgb>
2018-12-26 15:24:48 -0800

    Add a todo to gifts file

--- src/components/Gifts/index.js
+++ src/components/Gifts/index.js
@@ -5,7 +5,10 @@ const Gifts = () => {
   return(
     <div>
       <h1>Gifts</h1>
+      // TODO: ADD LINK
+      // <a href='' target='_blank' rel='nofollow'>
       <Button text='Registry this way'/>
+      // </a>
     </div>
   )
 }
